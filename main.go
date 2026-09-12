8f99717d47131908f4828202f7b687dbb7040637
da123456794 <3180201598@qq.com>
2026-09-12 12:47:34 +0800

    fix: 修复main.go配置里的语言可能不受支持

--- main.go
+++ main.go
@@ -8,7 +8,15 @@ import (
 
 // main 主函数
 func main() {
+	// 加载配置
 	config.Load()
+	// 配置里的语言可能不受支持
+	if !i18n.IsSupported(config.Get().Language) {
+		// 校正一下
+		config.SetLanguage(config.DefaultLanguage)
+	}
+	// 加载语言包
 	_ = i18n.Load(config.Get().Language)
+	// 运行菜单
 	menu.Run()
 }
